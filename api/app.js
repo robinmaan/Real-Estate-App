5559afd838710d761e126311d1dec2ae295e31fa
robin maan <robinmaan8055@gmail.com>
2024-04-26 21:29:11 +0530

    first commit

--- api/app.js
+++ api/app.js
@@ -1,8 +1,9 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import PostRoute from './routes/post.route.js'
 import authRoute from './routes/auth.route.js'
 const app = express()
-
+app.use(cookieParser())
 app.use(express.json())
 app.use('/api/posts',PostRoute)
 app.use('/api/auth',authRoute)
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,78 @@
 import bcrypt from 'bcrypt'
+import prisma from '../lib/prisma.js';
+import jwt from 'jsonwebtoken'
+
+
 
 export const register = async(req,res)=>{
     const {username,email,password}= req.body;
 
     //HASHING PASSWORD
-  const hashedPassword = await bcrypt.hash(password,10) 
-  console.log(hashedPassword)
-    //CREATING NEW USER
-}
-export const login = (req,res)=>{
+ 
+    try {
+        // HASH THE PASSWORD
+    
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        console.log(hashedPassword);
+    
+        // CREATE A NEW USER AND SAVE TO DB
+        const newUser = await prisma.user.create({
+          data: {
+            username,
+            email,
+            password: hashedPassword,
+          },
+        });
+    
+        console.log(newUser);
+    
+        res.status(201).json({ message: "User created successfully" });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Failed to create user!" });
+      }
+    };
+    
+
+export const login = async(req,res)=>{
+  const {username , password } = req.body
     //db operations
+    try {
+      //CHECK IF THE USER IS EXIST 
+      const user = await prisma.user.findUnique({
+        where:{username}
+      })
+      if(!user) res.status(401).json({message:"Undefined"})
+
+    //CHECK THE PASSWORD IS CORRECT
+    const isPasswordCorrect = await bcrypt.compare(password,user.password)
+    if(!isPasswordCorrect) res.status(401).json({message:"invlid password"})
+
+      
+    const age = 1000 * 60 * 60 *24 *7;
+    //GENERATE THE TOKENS ANS//SEND THE TOKEN TO THE USER
+    //  res.setHeader("Set-Cookie","test=" + "myValue").json("success")
+      const token = jwt.sign({
+        id:user.id
+      },process.env.SECRET_KEY,{
+        expiresIn:age
+      });
+
+
+    res.cookie("token",token,{
+      HttpOnly:true,
+      secure:true,
+      maxAge:age
+    }).status(200).json({message:"success login"})
+
+
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({message:"Failed to login!"})
+    }
+    
 }
 export const logout = (req,res)=>{
-    //db operations
+    res.clearCookie("token").status(200).json({message:"Logout successfull"})
 }
