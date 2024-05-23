149eb457120ed314e47b4ec81f7b55c8c665f205
robin maan <robinmaan8055@gmail.com>
2024-05-23 23:34:36 +0530

    completion

--- api/app.js
+++ api/app.js
@@ -2,9 +2,15 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import PostRoute from './routes/post.route.js'
 import authRoute from './routes/auth.route.js'
+import cors from 'cors'
+
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
+app.use(cors({origin: process.env.CLIENT_URL,credentials:true}))
+//credentials: true: This option tells the server to include credentials
+// (cookies, authorization headers, etc.) in the CORS requests.
+// This is necessary if your application needs to send or receive cookies across origins.
 app.use('/api/posts',PostRoute)
 app.use('/api/auth',authRoute)
 
